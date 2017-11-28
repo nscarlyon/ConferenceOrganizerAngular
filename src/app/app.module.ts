45a63958194bf4f3e89b062e0418abcd98615181
nscarlyon <natasha.carlyon@gmail.com>
2017-11-27 22:40:31 -0500

    add speaker and admin components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SpeakerComponent } from './speaker/speaker.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    SpeakerComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
