697625b745ad46b53d5bdc0ae1be2971728afe80
nscarlyon <natasha.carlyon@gmail.com>
2018-01-25 17:41:39 -0500

    add RouterTestingModule

--- src/app/admin/admin-home/admin-home.component.spec.ts
+++ src/app/admin/admin-home/admin-home.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminHomeComponent } from './admin-home.component';
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('AdminHomeComponent', () => {
   let component: AdminHomeComponent;
@@ -8,7 +9,8 @@ describe('AdminHomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminHomeComponent ]
+      declarations: [ AdminHomeComponent ],
+      imports: [RouterTestingModule]
     })
     .compileComponents();
   }));
