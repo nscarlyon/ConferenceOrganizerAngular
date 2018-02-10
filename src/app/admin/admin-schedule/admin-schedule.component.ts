27cd80a770a0ecd59733d35cc67bfda2771e096f
nscarlyon <natasha.carlyon@gmail.com>
2018-02-10 18:07:15 -0500

    navigate to sessions page from schedule

--- src/app/admin/admin-schedule/admin-schedule.component.ts
+++ src/app/admin/admin-schedule/admin-schedule.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-admin-schedule',
@@ -6,10 +7,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-schedule.component.css']
 })
 export class AdminScheduleComponent implements OnInit {
+  schedule: any;
 
-  constructor() { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.schedule = {
+      rooms: ["Room A", "Room B", "Room C"],
+      timeSlots: [
+        {
+          timeSlot: "8:00 - 9:00",
+          sessions: [
+            {
+              speakerName: "Jane Austen",
+              title: "Pride & Prejudice",
+              room: "Room A",
+              startHour: 8,
+              startMin: 0,
+              endHour: 10,
+              endMin: 0
+            },
+            {
+            },
+            {
+              speakerName: "Oscar Wilde",
+              title: "Picture of Dorian Grey",
+              room: "Room B",
+              startHour: 8,
+              startMin: 0,
+              endHour: 9,
+              endMin: 0
+            }
+          ]
+        },
+        {
+          timeSlot: "9:00 - 10:00",
+          sessions: [
+            {
+
+            },
+            {
+              speakerName: "Kent Beck",
+              title: "Extreme Programming",
+              room: "Room B",
+              startHour: 9,
+              startMin: 0,
+              endHour: 10,
+              endMin: 0
+            },
+            {
+
+            }
+            ]
+        }
+      ]
+    }
+
+  }
 
   ngOnInit() {
   }
 
+  goToSessionPage(id: number): void {
+    this.router.navigate([`../sessions/${id}`], {relativeTo: this.activatedRoute})
+  }
 }
