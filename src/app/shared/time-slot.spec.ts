e4c6772a2a0e17827c3054d45659da6b97b94fa1
nscarlyon <natasha.carlyon@gmail.com>
2018-05-03 19:30:40 -0400

    test more timeslot use cases

--- src/app/shared/time-slot.spec.ts
+++ src/app/shared/time-slot.spec.ts
@@ -1,29 +1,35 @@
 import {TimeSlot} from "./time-slot";
 describe("Time Slot Tests", () => {
 
-  it("should set all military times after 11 for TimeSlot with start and ending time", () => {
-    let timeSlot: TimeSlot = new TimeSlot("13:00", "14:00");
+  it("should set timeslot with corresponding military times", () => {
+    let timeSlot: TimeSlot = new TimeSlot("13:15", "14:15");
     expect(timeSlot.startHour).toEqual(13);
-    expect(timeSlot.startMin).toEqual(0);
+    expect(timeSlot.startMin).toEqual(15);
     expect(timeSlot.endHour).toEqual(14);
-    expect(timeSlot.endMin).toEqual(0);
+    expect(timeSlot.endMin).toEqual(15);
   });
 
-  it("should set all military times before 11 for TimeSlot with start and ending time", () => {
-    let timeSlot: TimeSlot = new TimeSlot("08:30", "09:30");
+  it("should set timeslot with all military times with initials 0s", () => {
+    let timeSlot: TimeSlot = new TimeSlot("08:05", "09:01");
     expect(timeSlot.startHour).toEqual(8);
-    expect(timeSlot.startMin).toEqual(30);
+    expect(timeSlot.startMin).toEqual(5);
     expect(timeSlot.endHour).toEqual(9);
-    expect(timeSlot.endMin).toEqual(30);
+    expect(timeSlot.endMin).toEqual(1);
   });
 
-  it("should convert from military time before 1PM to standard time", () => {
-    let timeSlot: TimeSlot = new TimeSlot("8:00", "9:00");
-    expect(timeSlot.standardTime).toEqual("8:00-9:00 A.M");
+  it("should convert military times before 1PM to standard time", () => {
+    let timeSlotOne: TimeSlot = new TimeSlot("10:00", "11:00");
+    let timeSlotTwo: TimeSlot = new TimeSlot("11:00", "12:00");
+    let timeSlotThree: TimeSlot = new TimeSlot("12:00", "12:59");
+    expect(timeSlotOne.standardTime).toEqual("10:00-11:00 A.M");
+    expect(timeSlotTwo.standardTime).toEqual("11:00-12:00 P.M");
+    expect(timeSlotThree.standardTime).toEqual("12:00-12:59 P.M");
   });
 
-  it("should convert from military time after 12PM to standard time", () => {
-    let timeSlot: TimeSlot = new TimeSlot("13:00", "14:00");
-    expect(timeSlot.standardTime).toEqual("1:00-2:00 P.M");
+  it("should convert military times after 12PM to standard time", () => {
+    let timeSlotOne: TimeSlot = new TimeSlot("13:00", "14:00");
+    let timeSlotTwo: TimeSlot = new TimeSlot("22:30", "24:30");
+    expect(timeSlotOne.standardTime).toEqual("1:00-2:00 P.M");
+    expect(timeSlotTwo.standardTime).toEqual("10:30-12:30 P.M");
   });
 });
